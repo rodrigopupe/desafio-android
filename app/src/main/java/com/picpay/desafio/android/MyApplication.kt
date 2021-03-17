c6c5a6f87a274f8add56f2adf32f2bb56c1dbead
Rodrigo Pupe <rodrigopupe@outlook.com>
2021-03-16 22:42:35 -0300

    Feat: Adding an Application class.

---
+++ app/src/main/java/com/picpay/desafio/android/MyApplication.kt
@@ -0,0 +1,24 @@
+package com.picpay.desafio.android
+
+import android.app.Application
+import com.picpay.desafio.android.di.networkModule
+import com.picpay.desafio.android.di.repositoryModule
+import com.picpay.desafio.android.di.useCaseModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MyApplication)
+            modules(
+                networkModule,
+                repositoryModule,
+                useCaseModule
+            )
+        }
+    }
+}
